bd51ef97652c0e5a0e635bf2d8899137e4be8c9d
alemyt89-star <alemyt89@gmail.com>
2026-03-12 16:06:47 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,30 @@
-const CACHE_NAME = 'staffalpine-v1';
+const CACHE_NAME = 'staffalpine-v3';
 const urlsToCache = [
-  '/',
   '/index.html',
   '/dashboard-lavoratore.html',
   '/dashboard-azienda.html',
-  '/login.html'
+  '/login.html',
+  '/recensione.html',
+  '/esplora-aziende.html'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
